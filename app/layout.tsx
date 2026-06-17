cb37c13699c545afec6453497f2bb377ba401023
erenarslan <hassan67844@gmail.com>
2026-06-17 21:18:09 +0300

    Add noindex, nofollow to preview version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -10,6 +11,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  robots: "noindex, nofollow",
+};
 
 export default async function RootLayout({
   children,
